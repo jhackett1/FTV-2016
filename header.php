9c830252f1d416223f5016144ac9c88c67165c00
jhackett1 <hello@joshuahackett.com>
2016-03-12 00:42:04 +0000

    Minor changes
    More CSS especially

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
   <title><?php wp_title(); ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+  <script src="http://localhost/wp/wp-content/themes/FTV%202016/js/scroll.js"></script>
 
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <section id="hero">
+
   <img id="logo" src="<?php bloginfo('template_directory'); ?>/img/logo.png" />
   <div class="divider"></div>
   <span id="slogan"><?php bloginfo( 'description' ); ?></span>
@@ -14,7 +15,6 @@
 <section id="latest">
 
   <?php
-
   //Declare counter variable
   $counter = 0;
 
@@ -23,30 +23,56 @@
   		the_post();
         ?>
 
-        <?php if ($counter===0){ ?>
+        <?php
 
-          <!-- The most recent video -->
-          <div class="tile">
-            <?php the_title(); ?>
-          </div>
+          $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+
+        if ($counter<4){
+
+         if ($counter===0){ ?>
+            <!-- The most recent video -->
+
+            <div class="tile" id="first">
+              <h5><?php the_category(); ?></h5>
+              <h3><?php the_title(); ?></h3>
+              <p><?php the_excerpt(); ?></p>
+              <div class="triangle"></div>
+            </div>
+
+            <div class="tile" style="background-image: url(<?php echo $url; ?>)">
+            </div>
+
+        <?php } elseif ($counter===3) { ?>
+
+            <!-- All other videos -->
+            <div class="tile" style="background-image: url(<?php echo $url; ?>)">
+            </div>
+
+            <div class="tile" id="first">
+              <h5><?php the_category(); ?></h5>
+              <h3><?php the_title(); ?></h3>
+              <p><?php the_excerpt(); ?></p>
+              <div class="triangle-reverse"></div>
+            </div>
 
         <?php } else { ?>
 
-          <!-- All other videos -->
-          <div class="tile">
-            <?php the_title(); ?>
-            <?php echo $counter; ?>
+          <div class="tile" style="background-image: url(<?php echo $url; ?>)">
           </div>
 
-        <?php };
-
+        <?php } }
         //Iterate the counter
         $counter++;
-
   	} // end while
   } // end if
   ?>
+</section>
 
+
+<section id="contact">
+  <h5>
+    <a href="mailto:forgetv@forgetoday.com">forgetv@forgetoday.com</a>
+  </h5>
 </section>
 
 <?php get_footer(); ?>
